2efe1be9683397c3951a6e861bb73b3b16fa0085
assalebrahim <afsalibrahimpb5@gmail.com>
2026-01-04 12:41:08 +0400

    Add sales modules

---
+++ frontend/BaseTrade-Frontend/src/pages/sales/Quotation.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Quotation = () => {
+    return (
+        <div className='p-10 flex justify-between'>
+            <h2 className='module-heading'>Quotation</h2>
+            <div className='w-50 flex justify-between'>
+                <span className='module-border py-2 px-4 items-center justify-start cursor-pointer'>...</span>
+                <button className='text-white bg-black rounded-md p-2 cursor-pointer'>+Add Quotation</button>
+            </div>
+        </div>
+    )
+}
+
+export default Quotation
